fb47f082a3c013d569ead45b6a7b9dacbc0fd86a
Erwin Acher <erwin.acher@gmail.com>
2025-10-24 18:11:38 +0100

    build pipeline in progress

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
+import tensor;
 #include <print>
 #include <version>
 
 int main() {
     std::println("hello, from {}", __cplusplus);
+
+    // testing modules c++23
+    int result = tensor::add_tensors(10, 20);
+    std::println("Tensor add result = {}", result);
+
 #ifdef __cpp_lib_print
     std::println("std::print() is available!");
 #endif
---
+++ src/tensor/tensor.ixx
@@ -0,0 +1,5 @@
+export module tensor;
+
+export int add_tensors(int a, int b) {
+    return a + b;
+}
